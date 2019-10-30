6346db4e6ad07325abb93de6ca74f50bd9457a39
Matthäus Heer <maheer@student.ethz.ch>
2019-10-30 16:46:53 +0100

    Working.

--- planvec/gui/gui_config.py
+++ planvec/gui/gui_config.py
@@ -10,21 +10,26 @@ config_dict = {
         'width': 1700,
         'height': 700
     },
+    'video': {
+        'camera': 'USB',  # either 'USB' or 'BUILTIN'
+        'max_input_width': 1920,  # high res: 1920, low res: 640
+        'max_input_height': 1080,  # high res: 1080, low res: 480
+        'display_width': 850,
+        'display_height': 900
+    },
     'color_range': {
         'blue':     HSVColorRange([91,  66,  20],  [160, 255, 255]),
         'red_low':  HSVColorRange([0,   71,  71],  [5,   255, 255]),
         'red_high': HSVColorRange([165, 60,  60],  [179, 255, 255]),
         'green':    HSVColorRange([43,  87,  30],  [90,  225, 225])
     },
-    'video': {
-        'camera': 'USB',  # either 'USB' or 'BUILTIN'
-        'max_input_width': 1920,
-        'max_input_height': 1080,
-        'display_width': 850,
-        'display_height': 900
-    },
-    'style': {
-
+    'processing': {
+        'rectify_shape': (600, 350),
+        'gaussian_blur': (3, 3),
+        'img_threshold': (110, 255),
+        'area_threshold': 5000,
+        'contours_n_points_threshold': 100,
+        'polygon_tolerance': 1
     }
 }
 
--- planvec/pipeline.py
+++ planvec/pipeline.py
@@ -7,7 +7,7 @@ from planvec import color_range
 from planvec import vizualization
 from planvec import conversions
 
-HELPER_COLOR_RANGES = [color_range.BLUE, color_range.RED_HIGH, color_range.RED_LOW, color_range.GREEN]
+HELPER_COLOR_RANGES = [color_range.BLUE, color_range.RED_HIGH, color_range.RED_LOW]
 DEFAULT_FIG_SIZE = (13, 8)
 
 
@@ -20,7 +20,7 @@ def run_pipeline(img, visualize_steps=False, verbose=False, return_np_arr=True, 
     if visualize_steps:
         vizualization.imshow(img, axis='off', figsize=DEFAULT_FIG_SIZE, img_space='BGR')
     # Process image, stretch to dots, and filter out helper colors
-    img = img_proc.rectify_wrt_red_dots(img, (600, 350), show_plot=visualize_steps, verbose=verbose)
+    img = img_proc.rectify_wrt_red_dots(img, (1920, int(1920 * 0.7)), show_plot=visualize_steps, verbose=verbose)
     img = img_proc.add_gaussian_blur(img, 3, 3)
     img = img_proc.filter_multi_hsv_ranges_to_white(img, HELPER_COLOR_RANGES)
     if visualize_steps:
@@ -37,8 +37,6 @@ def run_pipeline(img, visualize_steps=False, verbose=False, return_np_arr=True, 
         print(f'Found {n} regions.')
     img_labelled_proc, filtered_regions = img_proc.filter_regions(labelled_img, regionprops=regions,
                                                                   area_threshold=5000, verbose=verbose)
-    if verbose:
-        print(f'After filtering, {len(filtered_regions)} are left.')
     if visualize_steps:
         vizualization.imshow(img_labelled_proc, axis='off', figsize=DEFAULT_FIG_SIZE, img_space='BGR')
         vizualization.plot_image_regions(img_labelled_proc, filtered_regions, figsize=DEFAULT_FIG_SIZE)
@@ -50,18 +48,17 @@ def run_pipeline(img, visualize_steps=False, verbose=False, return_np_arr=True, 
     # Approximate contours by polygons to smooth outline
     approx_contours = []
     for contour in contours:
-        approx_contours.append(skimage.measure.approximate_polygon(contour.copy(), tolerance=1))
+        approx_contours.append(skimage.measure.approximate_polygon(contour.copy(), tolerance=0.7))
 
     output_fig = vizualization.plot_contours(approx_contours, axis='off', color='red', linewidth=0.5,
                                              figsize=DEFAULT_FIG_SIZE)
-    output_fig.set_size_inches(6, 5)
+    output_fig.set_size_inches(7.87402, 5.51181)
 
     # Important: Close the figure, else a shitton of figures are staying open
     #            which will fill up memory.
     pil_img = conversions.fig2img(output_fig)
     if not return_fig:
         plt.close(output_fig)
-
         return ImageQt(pil_img)
     else:
         print(plt.get_fignums())
