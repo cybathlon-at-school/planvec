6d1ed94a8f7ba63debf825e32cb02562e0850071
Matthaeus Heer <matthaeusheer@gmail.com>
2022-01-21 17:16:20 +0100

    cleaned up setup

--- planvec/gui/RuntimeStyleSheets.py
+++ planvec/gui/RuntimeStyleSheets.py
@@ -19,27 +19,29 @@ class WrappedMainWindowWithStyle(QMainWindow, QtStyleTools):
         self.ui.logo.setProperty('class', 'logo')
         self.ui.infoBox.setProperty('class', 'info-box')
         self.ui.settingsBox.setProperty('class', 'settings-box')
-        self.ui.settingsAdvanced.setProperty('class', 'settings-box')
+        #self.ui.settingsAdvanced.setProperty('class', 'settings-box')
         self.ui.infoBoxInput.setProperty('class', 'info-box-input')
         self.ui.infoBoxOutput.setProperty('class', 'info-box-input')
 
         q_app.installEventFilter(self)
 
-        QTimer.singleShot(0, self.ui.settingsAdvanced.hide)  # hide the advanced setting module
+        #QTimer.singleShot(0, self.ui.settingsAdvanced.hide)  # hide the advanced setting module
 
-        self.ui.advancedOpenButton.setMouseTracking(1)
-        self.ui.advancedCloseButton.setMouseTracking(1)
+        #self.ui.advancedOpenButton.setMouseTracking(1)
+        #self.ui.advancedCloseButton.setMouseTracking(1)
 
         self.quitShortcut = QShortcut(QKeySequence('Ctrl+Q'), self)
         self.quitShortcut.activated.connect(QApplication.instance().quit)
 
     def eventFilter(self, source, event):
         if event.type() == QEvent.MouseButtonPress:
+            """
             if source == self.ui.advancedOpenButton:
                 self.ui.settingsAdvanced.show()
                 self.ui.settingsAdvanced.setGeometry(10, 296, 261, 449)
 
             if source == self.ui.advancedCloseButton:
                 self.ui.settingsAdvanced.hide()
+            """
 
         return QMainWindow.eventFilter(self, source, event)
